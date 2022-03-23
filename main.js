723152504249501f5c7ae4f6a4165b1f75fb21de
MunimR <munim.rah123456@gmail.com>
2022-03-23 18:32:42 -0400

    updates to content and JS

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
+const checkElement = async selector => {
+    while ( document.querySelector(selector) === null) {
+      await new Promise( resolve =>  requestAnimationFrame(resolve) )
+    }
+    return document.querySelector(selector); 
+  };
+  
+
 function load() {
-    var c = document.querySelectorAll(".ani");
+    var h = location.hash;
+    if (h == "") {
+        h = "#home";
+    }
+    checkElement(h).then((selector) => {
+        var c = document.querySelector(h).querySelectorAll(".ani");
+
+    document.title = h;
+
     for (const x of c) {
         x.classList.add("loaded");
     }
+      });
+
+    
 }
 
 
-window.onload = function(){
-    load();
-}
+window.onhashchange = load;
+
+window.onload = load;
