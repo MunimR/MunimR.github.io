a036135b40bfa3867bd82cf2809c71709add88b0
MunimR <munim.rah123456@gmail.com>
2022-03-23 21:59:07 -0400

    Finalized major content and updated hierarchy

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const checkElement = async selector => {
     while ( document.querySelector(selector) === null) {
-      await new Promise( resolve =>  requestAnimationFrame(resolve) )
+        await new Promise( resolve =>  requestAnimationFrame(resolve) )
     }
     return document.querySelector(selector); 
-  };
-  
+};
 
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+  
 function load() {
     var h = location.hash;
     if (h == "") {
@@ -13,17 +16,18 @@ function load() {
     }
     checkElement(h).then((selector) => {
         var c = document.querySelector(h).querySelectorAll(".ani");
+        document.title = capitalizeFirstLetter(h.substring(1));
 
-    document.title = h;
-
-    for (const x of c) {
-        x.classList.add("loaded");
-    }
-      });
-
-    
+        for (const x of c) {
+            x.classList.add("loaded");
+        }
+    });
 }
 
+function email() {
+    navigator.clipboard.writeText("munimrahm@gmail.com");
+    alert("My email: munimrahm@gmail.com has been copied to your clipboard")
+}
 
 window.onhashchange = load;
 
